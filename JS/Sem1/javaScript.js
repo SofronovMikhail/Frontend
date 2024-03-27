c3fdf029f43e6d9a76308c6589c8046e6bb4f825
«SofronovMikhail». <MishaSof2008@yandex.ru>
2024-03-28 00:02:02 +0300

    Sem1

---
+++ JS/Sem1/javaScript.js
@@ -0,0 +1,9 @@
+let degrees = prompt("Введите температуру в градусах Цельсия: ", " ");
+alert(`Градусы Цельсия: ${degrees}`);
+alert(`Градусы Фаренгейта: ${degrees * 1.8 + 32}`);
+console.log(`Градусы Цельсия: ${degrees}`);
+console.log(`Градусы Фаренгейта: ${degrees * 1.8 + 32}`);
+
+const name = "Михаил";
+let admin = name;
+console.log(admin);
