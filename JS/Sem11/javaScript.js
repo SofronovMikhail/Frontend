837f3c3a140cd2353f8177b3696868cfd8fa79cc
SofronovMikhail <MishaSof2008@yandex.ru>
2024-04-28 21:42:17 +0300

    Sem11 Complete

--- JS/Sem11/javaScript.js
+++ JS/Sem11/javaScript.js
@@ -15,40 +15,39 @@
 Реализуйте конструктор, который принимает начальный список книг (массив) в качестве аргумента. Убедитесь, что предоставленный массив не содержит дубликатов; в противном случае выбрасывайте ошибку.
 */
 class ArrayBook{
-    #book = [];
+    #books = [];
     constructor(name1, name2, name3){
-        this.#book.push(name1, name2, name3);   
-    }
-    getBooks(){
-        this.#book.forEach(element => {console.log(element);});
-    }
-    setBook(nameBook){
-        const hasNameBook = this.#book.includes(nameBook);
+        this.#books.push(name1, name2, name3);
+    };
+    allBook(){
+        this.#books.forEach((element) => console.log(element));
+    };
+    addBook(title){
+        const hasNameBook = this.#books.includes(title);
         if (hasNameBook === true) {
             throw new Error("Book is has");
         } 
-        this.#book.push(nameBook);
+        this.#books.push(title);
+    };
+    hasBook(title){
+        console.log(this.#books.includes(title)); 
     }
-    hasBook(nameBook){
-        const hasNameBook = this.#book.includes(nameBook);
-        console.log(hasNameBook); 
-    }
-    deleteBook(nameBook){
-        if (this.#book.includes(nameBook)) {
-           this.#book.pop(nameBook);
+    removeBook(title){
+        const hasNameBook = this.#books.includes(title);
+        if (hasNameBook === true){
+           this.#books = this.#books.filter((el) => el !== title);
         }  
         else{
             throw new Error("Book is not has");
-        } 
-        
+        }  
     }
 }
 const arrayBook = new ArrayBook("Война и Мир", "Обломов", "Гранатовый браслет")
-arrayBook.getBooks();
-arrayBook.setBook("На дне");
-arrayBook.deleteBook("Обломов");
+arrayBook.allBook();
+arrayBook.addBook("На дне");
+arrayBook.removeBook("Обломов");
 arrayBook.hasBook("Гранатовый браслет");
-arrayBook.getBooks();
+arrayBook.allBook();
 /*
 Задание 2
 Вы разрабатываете систему отзывов для вашего веб-сайта. Пользователи могут оставлять отзывы, но чтобы исключить слишком короткие или слишком длинные сообщения, вы решаете установить некоторые ограничения.
@@ -58,40 +57,20 @@ arrayBook.getBooks();
 Напишите функцию, которая будет добавлять отзыв в контейнер с отзывами. Однако если длина введенного отзыва менее 50 или более 500 символов, функция должна генерировать исключение.
 
 При добавлении отзыва, он должен отображаться на странице под предыдущими отзывами, а не заменять их.
-
-const initialData = [
-{
-product: "Apple iPhone 13",
-reviews: [
-{
-id: "1",
-text: "Отличный телефон! Батарея держится долго.",
-},
-{
-id: "2",
-text: "Камера супер, фото выглядят просто потрясающе.",
-},
-],
-},
-{
-product: "Samsung Galaxy Z Fold 3",
-reviews: [
-{
-id: "3",
-text: "Интересный дизайн, но дорогой.",
-},
-],
-},
-{
-product: "Sony PlayStation 5",
-reviews: [
-{
-id: "4",
-text: "Люблю играть на PS5, графика на высоте.",
-},
-],
-},
-];
-
-Вы можете использовать этот массив initialData для начальной загрузки данных при запуске вашего приложения.
 */
+let arr = [];
+let button = document.querySelector('.button');
+let displayText = document.querySelector('.comment')
+
+button.onclick = function(){
+  const text = document.querySelector('.textarea_review').value;
+  if(text.length < 50 || text.length > 500) {
+    document.querySelector('.textarea_review').value = " ";
+    throw new Error("Отзыв не принят");
+  }
+  arr.push(document.querySelector('.textarea_review').value);
+  const paragraf = document.createElement("p"); 
+  paragraf.innerText = document.querySelector('.textarea_review').value;;
+  displayText.appendChild(paragraf);
+  document.querySelector('.textarea_review').value = " ";
+}  
