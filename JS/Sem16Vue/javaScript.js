221113b78726bfe1b704280af4bd0b3277a3bfc3
SofronovMikhail <MishaSof2008@yandex.ru>
2024-05-19 21:29:27 +0300

    Sem16Vue Complete task2

---
+++ JS/Sem16Vue/javaScript.js
@@ -0,0 +1,82 @@
+
+new Vue({
+    el: "#app",
+    data: {
+        lists: [
+            {
+                src: "../imgs/artikle_news_img1.png",
+                title: "Создадим лучший макет перепланировки",
+                date: "26 Декабрь,2022",
+                xmlns: "http://www.w3.org/2000/svg",
+                content: "Дизайн кухни",
+            },
+            {
+                src: "../imgs/artikle_news_img2.png",
+                title: "Лучшие интерьерные идеи по низкой цене",
+                date: "22 Декабрь,2022",
+                xmlns: "http://www.w3.org/2000/svg",
+                content: "Дизайн для жизни",
+            },
+            {
+                src: "../imgs/artikle_news_img3.png",
+                title: "Лучшие интерьерные решения для офисов",
+                date: "25 Декабрь,2022",
+                xmlns: "http://www.w3.org/2000/svg",
+                content: "Дизайн Интерьера",
+            },
+            {
+                src: "../imgs/artikle_news_img4.png",
+                title: "Создадим лучший макет перепланировки",
+                date: "26 Декабрь,2022",
+                xmlns: "http://www.w3.org/2000/svg",
+                content: "Дизайн кухни",
+            },
+            {
+                src: "../imgs/artikle_news_img5.png",
+                title: "Лучшие интерьерные идеи по низкой цене",
+                date: "22 Декабрь,2022",
+                xmlns: "http://www.w3.org/2000/svg",
+                content: "Дизайн для жизни",
+            },
+            {
+                src: "../imgs/artikle_news_img6.png",
+                title: "Лучшие интерьерные решения для офисов",
+                date: "25 Декабрь,2022",
+                xmlns: "http://www.w3.org/2000/svg",
+                content: "Дизайн Интерьера",
+            },
+         ],
+        /*[
+            `<div class="article_news_item">
+            <img class="article_news_item_img" v-bind:src="item.src" alt="Картинка">
+            <a href="#" class="artikle_news_item_title">"{{ item.title }}</a>
+            <div class="artikle_news_item_dates">
+                <p class="artikle_news_item_date">{{ item.date }}</p>
+                <div class="article_news_item_svg">
+                    <svg width="9"  class="news_swg" height="16" viewBox="0 0 9 16" fill="none" v-bind:xmlns="item.xmlns">
+                        <path d="M1.77142 14.9527L7.71428 8.267L1.77142 1.58128" stroke="#292F36" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                </div>
+            </div>
+            <div class="flow_box">
+                <p class="flow_box_content">{{ item.content }}</p>
+            </div>
+        </div>`,
+        `<div class="article_news_item">
+        <img class="article_news_item_img" v-bind:src="item.src" alt="Картинка">
+        <a href="#" class="artikle_news_item_title">"{{ item.title }}</a>
+        <div class="artikle_news_item_dates">
+            <p class="artikle_news_item_date">{{ item.date }}</p>
+            <div class="article_news_item_svg">
+                <svg width="9"  class="news_swg" height="16" viewBox="0 0 9 16" fill="none" v-bind:xmlns="item.xmlns">
+                    <path d="M1.77142 14.9527L7.71428 8.267L1.77142 1.58128" stroke="#292F36" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+            </div>
+        </div>
+        <div class="flow_box">
+            <p class="flow_box_content">{{ item.content }}</p>
+        </div>
+    </div>`,
+         ]*/
+    },
+});
