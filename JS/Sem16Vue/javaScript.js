7e7dc85ef5a3ec47f07fa3a125d6f00e6460517f
«SofronovMikhail». <MishaSof2008@yandex.ru>
2024-05-24 22:28:25 +0300

    Sem16vue task3 proccing

--- JS/Sem16Vue/javaScript.js
+++ JS/Sem16Vue/javaScript.js
@@ -45,38 +45,23 @@ new Vue({
                 xmlns: "http://www.w3.org/2000/svg",
                 content: "Дизайн Интерьера",
             },
-         ],
-        /*[
-            `<div class="article_news_item">
-            <img class="article_news_item_img" v-bind:src="item.src" alt="Картинка">
-            <a href="#" class="artikle_news_item_title">"{{ item.title }}</a>
-            <div class="artikle_news_item_dates">
-                <p class="artikle_news_item_date">{{ item.date }}</p>
-                <div class="article_news_item_svg">
-                    <svg width="9"  class="news_swg" height="16" viewBox="0 0 9 16" fill="none" v-bind:xmlns="item.xmlns">
-                        <path d="M1.77142 14.9527L7.71428 8.267L1.77142 1.58128" stroke="#292F36" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                </div>
-            </div>
-            <div class="flow_box">
-                <p class="flow_box_content">{{ item.content }}</p>
-            </div>
-        </div>`,
-        `<div class="article_news_item">
-        <img class="article_news_item_img" v-bind:src="item.src" alt="Картинка">
-        <a href="#" class="artikle_news_item_title">"{{ item.title }}</a>
-        <div class="artikle_news_item_dates">
-            <p class="artikle_news_item_date">{{ item.date }}</p>
-            <div class="article_news_item_svg">
-                <svg width="9"  class="news_swg" height="16" viewBox="0 0 9 16" fill="none" v-bind:xmlns="item.xmlns">
-                    <path d="M1.77142 14.9527L7.71428 8.267L1.77142 1.58128" stroke="#292F36" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-            </div>
-        </div>
-        <div class="flow_box">
-            <p class="flow_box_content">{{ item.content }}</p>
-        </div>
-    </div>`,
-         ]*/
+        ],
+        listDetails: [
+            {
+                nameTitle: "Кухня",
+            },
+            {
+                nameTitle: "Спальня",
+            },
+            {
+                nameTitle: "архитектура",
+            },
+            {
+                nameTitle: "Здание",
+            },
+            {
+                  nameTitle: "Ппланировка",
+            },
+        ],
     },
 });
