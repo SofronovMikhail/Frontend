45968517714b38f72e5c45bf88ac4d5b369abe74
SofronovMikhail <MishaSof2008@yandex.ru>
2024-05-28 23:09:29 +0300

    sem5Vue

---
+++ sem5/src/components/ProductDetails.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="products">
+    <div class="product">
+      <h3>{{ product.name }}</h3>
+      <p>{{ formattedPrice }}</p>
+      <p v-if="product.available">Available</p>
+      <p v-else>Out of stock</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetails',
+  data() {
+    return {
+      product: 
+        {
+          name: "Product",
+          price: 20,
+          available: true,
+        },
+    }
+  },
+  methods: {},
+  computed: {
+    formattedPrice: function(){
+      return `$${this.product.price}`
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
